8dac020cab326ba32b0e9efeabc8471537568462
bjwswang <bjwswang@gmail.com>
2023-11-30 09:36:26 +0000

    feat: add file type,size,count to knowledgebase
    Signed-off-by: bjwswang <bjwswang@gmail.com>

---
+++ pkg/utils/size_test.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2023 The KubeAGI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import "testing"
+
+func TestBytesToSize(t *testing.T) {
+	tests := []struct {
+		bytes    int64
+		expected string
+	}{
+		{1023, "1023 B"},
+		{1024, "1.00 KB"},
+		{5242880, "5.00 MB"},
+		{1073741824, "1.00 GB"},
+	}
+
+	for _, test := range tests {
+		result := BytesToSize(test.bytes)
+		if result != test.expected {
+			t.Errorf("BytesToSize(%d) = %s, expected %s", test.bytes, result, test.expected)
+		}
+	}
+}
